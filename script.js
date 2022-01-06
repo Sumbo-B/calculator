f79db8368fcd178164f6a8222f54ba9165063b57
Sumbo Bello <sumbobello@gmail.com>
2022-01-06 18:38:16 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,27 @@
+let display = document.getElementById("calc_display_digit");
+let buttons = Array.from(document.querySelectorAll(".calc_keys"));
+
+buttons.map((button) => {
+  button.addEventListener("click", (e) => {
+    switch (e.target.innerText) {
+      case "RESET":
+        display.innerText = "";
+        break;
+      case "DEL":
+        if (display.innerText) {
+          display.innerText = display.innerText.slice(0, -1);
+        }
+        break;
+      case "=":
+        try {
+          display.innerText = eval(display.innerText);
+        } catch {
+          display.innerText = "Error!";
+        }
+
+        break;
+      default:
+        display.innerText += e.target.innerText;
+    }
+  });
+});
